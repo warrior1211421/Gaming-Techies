efe814fe7b1bc360f25999dc350c4e496933ede0
Gaming Techies <82413248+gamingtechies54@users.noreply.github.com>
2021-11-14 15:40:01 +0500

    z

--- static/js/toc.js
+++ static/js/toc.js
@@ -75,3 +75,20 @@ document.addEventListener("DOMContentLoaded", () =>
         to: document.querySelector(".table-of-contents")
     }).generateToc()
 );
+
+$('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav',
+    autoplay: true,
+    autoplaySpeed: 3000
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    focusOnSelect: true,
+    arrows: false
+  });
