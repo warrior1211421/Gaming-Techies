cb57aea37dfd2502f49f6a10aad75f32c345b5f3
Gaming Techies <82413248+gamingtechies54@users.noreply.github.com>
2021-12-09 15:41:56 +0500

    s

--- static/js/toc.js
+++ static/js/toc.js
@@ -76,6 +76,25 @@ document.addEventListener("DOMContentLoaded", () =>
     }).generateToc()
 );
 
+  // Go to Top Button
+  $(document).ready(function() {
+    // Show or hide the sticky footer button
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 200) {
+            $('.go-top').fadeIn(200);
+        } else {
+            $('.go-top').fadeOut(200);
+        }
+    });
+    
+    // Animate the scroll to top
+$('.go-top').click(function(event) {
+        event.preventDefault();
+        
+        $('html, body').animate({scrollTop: 0}, 300);
+    })
+});
+
 $('.slider-for').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
